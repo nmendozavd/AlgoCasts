64b4f71b68ac84fdada12a566c1c144bbfc98d3d
Noel Mendoza <nmendozavd@gmail.com>
2020-06-10 01:43:26 -0700

    first attempt

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,24 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+
+  // convert to string, splice?
+  let chunked = [];
+  
+  // if last element does not exist or equals size
+
+  for (var i = 0; i < array.length; i++) {
+    let last = chunked[chunked.length - 1];
+    if (last === undefined || chunked.length === size) {
+      chunked.push([array[i]])
+    } else {
+      last.push(array[i])
+    }
+  }
+  
+  return chunked;
+
+}
 
 module.exports = chunk;
