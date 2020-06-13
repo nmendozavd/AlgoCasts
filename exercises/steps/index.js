29838108b4ddcd1cea7454875abc1f624c068a67
Noel Mendoza <nmendozavd@gmail.com>
2020-06-13 14:37:07 -0700

    completed exercises with two solutions

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,28 +17,48 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  
-  // from 0 to n (iterate through rows)
-  for (let row = 0; row < n; row++) {
-    // create an empty string stair
-    let stair = '';
-    // from 0 to n (iterate through columns)
-    for (let column = 0; column < n; column++) {
-      // if current column is equal to or less than row
-      if (column <= row) {
-        // add a # to stair
-        stair += '#'
-      } else {
-        // otherwise add a space
-        stair += ' ';
-      }
-    }
-    // console log stair inside for loop
-    console.log(stair); 
+// recursion solution
+function steps (n, row = 0, stair = '') {
+  if (n === row) {
+    return;
   }
-  
-  
+
+  if (n === stair.length) {
+    console.log(stair);
+    steps(n, row + 1);
+    return;
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+
+  steps(n, row, stair);
+
 }
+
+// function steps(n) {
+
+//   // from 0 to n (iterate through rows)
+//   for (let row = 0; row < n; row++) {
+//     // create an empty string stair
+//     let stair = '';
+//     // from 0 to n (iterate through columns)
+//     for (let column = 0; column < n; column++) {
+//       // if current column is equal to or less than row
+//       if (column <= row) {
+//         // add a # to stair
+//         stair += '#'
+//       } else {
+//         // otherwise add a space
+//         stair += ' ';
+//       }
+//     }
+//     // console log stair inside for loop
+//     console.log(stair);
+//   }
+// }
 
 module.exports = steps;
