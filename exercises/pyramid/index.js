ee020939e23dab97349ebea65b0da59bc8fae849
Noel Mendoza <nmendozavd@gmail.com>
2020-06-13 17:06:15 -0700

    completed exercise with one solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,25 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  // get midPoint of array for each row
+  const midPoint = Math.floor((2 * n - 1) / 2);
+  // from o to n (iterate through rows)
+  for (let row = 0; row < n; row++) {
+    // create an empty string, 'level'
+    let level = '';
+    // from 0 to two times n - 1 
+    for (let column = 0; column < (n * 2) - 1; column++) {
+      // 
+      if (midPoint - row <= column && midPoint + row >= column) {
+        level += '#';
+      } else { // otherwise add space to 'level'
+        level += ' ';
+      }
+    }
+    //console.log 'level'
+    console.log(level);
+  }  
+}
 
 module.exports = pyramid;
