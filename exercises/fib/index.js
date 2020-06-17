3759c4497f3bbcecb9a6d42654c7231d5f140a09
Noel Mendoza <nmendozavd@gmail.com>
2020-06-16 23:14:08 -0700

    completed exercise with two solutions

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,30 +8,52 @@
 // Example:
 //   fib(4) === 3
 
+
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  };
+}
+
+
 // solution 2 - Recursive
 function fib(n) {
-
-}
-
-// Runtime Complexity: 
-
-
-// solution 1 - iterable 
-function fib(n) {
-  // create array with first nums
-  let nums = [0,1];
-  // create fibonacci array up unitl n
-  for (let i = 1; i < n; i++) {
-    // logic to create next num, push to array
-    let a = nums[i];
-    let b = nums[i - 1];
-    nums.push(a + b);
+  if (n < 2) {
+    return n;
   }
-  // return last num in array, which is n
-  return nums[nums.length - 1];
+
+  return fib(n - 1) + fib(n - 2);
 }
 
-// Runtime Complexity: As n increases by 1, we have to do another calculation = linear 0(n)
+fib = memoize(fib);
+
+// Runtime Complexity: Exponential Time (every increase in N, dramatic increase in time) - No no 
+// Improve runtime with Memoization: store the arguments of each function call along with result. 
+
+
+// // solution 1 - iterable 
+// function fib(n) {
+//   // create array with first nums
+//   let nums = [0,1];
+//   // create fibonacci array up unitl n
+//   for (let i = 1; i < n; i++) {
+//     // logic to create next num, push to array
+//     let a = nums[i];
+//     let b = nums[i - 1];
+//     nums.push(a + b);
+//   }
+//   // return last num in array, which is n
+//   return nums[nums.length - 1];
+// }
+
+// // Runtime Complexity: As n increases by 1, we have to do another calculation = linear 0(n)
 
 
 
