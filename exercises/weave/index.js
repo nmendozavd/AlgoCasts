f588a0c33ef8963f802f70ab2a1dbdaee9d5c4db
Noel Mendoza <nmendozavd@gmail.com>
2020-06-17 22:09:04 -0700

    completed exercise

--- exercises/weave/index.js
+++ exercises/weave/index.js
@@ -24,6 +24,22 @@
 
 const Queue = require('./queue');
 
-function weave(sourceOne, sourceTwo) {}
+function weave(sourceOne, sourceTwo) {
+  // create new queue 
+  const q = new Queue();
+  // iterate through exisiting queues, make sure they aren't undefined
+  while (sourceOne.peek() !== undefined || sourceTwo.peek() !== undefined) {
+    // check each queue individually, and add record 
+    if (sourceOne.peek() !== undefined) {
+      q.add(sourceOne.remove());
+    }
+
+    if (sourceTwo.peek() !== undefined) {
+      q.add(sourceTwo.remove());
+    }
+  }
+  return q;
+
+}
 
 module.exports = weave;
--- exercises/weave/queue.js
+++ exercises/weave/queue.js
@@ -16,6 +16,11 @@ class Queue {
   remove() {
     return this.data.pop();
   }
+
+  peek() {
+    return this.data[this.data.length -1];
+  }
+
 }
 
 module.exports = Queue;
