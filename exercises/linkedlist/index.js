79814cd63672687bdc9e7f95e377470baeedd3b7
Noel Mendoza <nmendozavd@gmail.com>
2020-06-19 00:30:10 -0700

    completed size method of linked list

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,37 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor (data, next = null) {
+    this.data = data
+    this.next = next
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor () {
+    this.head = null
+  }
 
-module.exports = { Node, LinkedList };
+  insertFirst (data) {
+    const node = new Node(data, this.head)
+    this.head = node
+  }
+
+  size () {
+    // create counter var
+    let count = 0
+    let node = this.head
+    // run unitl end of linked list 
+    while (node !== null) {
+      // increment count
+      count++
+      // move onto next by assigning 'node' to next node
+      node = node.next
+    }
+
+    return count
+  }
+}
+
+module.exports = { Node, LinkedList}
