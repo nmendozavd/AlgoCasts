2ba56576769bcbf90c2746a8ec3f9a70e252ff60
Noel Mendoza <nmendozavd@gmail.com>
2020-06-19 17:12:55 -0700

    completed removeLast method

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -4,35 +4,99 @@
 
 class Node {
   constructor (data, next = null) {
-    this.data = data
-    this.next = next
+    this.data = data;
+    this.next = next;
   }
 }
 
 class LinkedList {
   constructor () {
-    this.head = null
+    this.head = null;
   }
 
   insertFirst (data) {
-    const node = new Node(data, this.head)
-    this.head = node
+    const node = new Node(data, this.head);
+    this.head = node;
   }
 
   size () {
-    // create counter var
-    let count = 0
-    let node = this.head
+    // create counter 
+    let count = 0;
+    let node = this.head;
     // run unitl end of linked list 
     while (node !== null) {
       // increment count
-      count++
+      count++;
       // move onto next by assigning 'node' to next node
-      node = node.next
+      node = node.next;
     }
 
-    return count
+    return count;
   }
+
+  getFirst () {
+    return this.head;
+  }
+
+  getLast () {
+    // edge case, with no nodes
+    if (this.head === undefined) {
+      return null;
+    }
+
+    let node = this.head
+    // iterate through linked list
+    while (node !== null) {
+      // if the next node points to null, we know it's the last node
+      if (node.next === null) {
+        // return the last node
+        return node;
+      }
+      // if not, keep  iterating 
+      node = node.next;
+    }
+  }
+
+  clear () {
+    this.head = null;
+  }
+
+  removeFirst () {
+    // edge case with no nodes
+    if (this.head === null) {
+      return;
+    }
+    // point this.head to the next node 
+    this.head = this.head.next;
+  }
+
+  removeLast () {
+    // edge case if linked list is empty
+    if (this.head === null) { 
+      return;
+    }
+    // edge case if only one node exists
+    if (this.head.next === null) {
+      // remove first node by pointing this.head to null
+      this.head = null;
+      // end method
+      return;
+    }
+    // node is set to second node
+    let node = this.head.next;
+    // previous is set to first node
+    let previous = this.head;
+    // while the next node is not null, we know it's not the last node 
+    while (node.next !== null) {
+      // set previous to node before node.next
+      previous = node; 
+      // set node to the next node
+      node = node.next;
+    }
+    // once node.next is null, we know it's the last, so set next to null to remove last node
+    previous.next = null;
+  }
+
 }
 
 module.exports = { Node, LinkedList}
--- exercises/linkedlist/test.js
+++ exercises/linkedlist/test.js
@@ -50,7 +50,7 @@ describe('GetFirst', () => {
   });
 });
 
-describe.skip('GetLast', () => {
+describe('GetLast', () => {
   test('returns the last element', () => {
     const l = new List();
     l.insertFirst(2);
@@ -60,7 +60,7 @@ describe.skip('GetLast', () => {
   });
 });
 
-describe.skip('Clear', () => {
+describe('Clear', () => {
   test('empties out the list', () => {
     const l = new List();
     expect(l.size()).toEqual(0);
@@ -74,7 +74,7 @@ describe.skip('Clear', () => {
   });
 });
 
-describe.skip('RemoveFirst', () => {
+describe('RemoveFirst', () => {
   test('removes the first node when the list has a size of one', () => {
     const l = new List();
     l.insertFirst('a');
@@ -97,7 +97,7 @@ describe.skip('RemoveFirst', () => {
   });
 });
 
-describe.skip('RemoveLast', () => {
+describe('RemoveLast', () => {
   test('RemoveLast removes the last node when list is empty', () => {
     const l = new List();
     expect(() => {
