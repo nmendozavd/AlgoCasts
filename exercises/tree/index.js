6cd45e9eab1c3d020f77b25213b191fd8bda0930
Noel Mendoza <nmendozavd@gmail.com>
2020-06-22 16:11:18 -0700

    completed exercises for tree

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,65 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+  constructor (data) {
+    this.data = data;
+    this.children = [];
+  }
 
-class Tree {}
+  add (data) {
+    // create new node, pass data
+    const node = new Node(data);
+    // push node into into children arrays
+    this.children.push(node);
+  }
 
-module.exports = { Tree, Node };
+  remove (data) {
+    //   // solution set children equal to filter method 
+    //  this.children = this.children.filter(node => {
+    //     // remove any node data that is not equal to data
+    //    return node.data !== data
+    //  }) 
+
+    // my solution using a for loop
+    for (let i = 0; i < this.children.length; i++) {
+      if (this.children[i].data === data) {
+        this.children.splice(this.children[i].data, 1);
+      }
+    }
+  }
+}
+
+class Tree {
+  constructor () {
+    this.root = null;
+  }
+
+  traverseBF (fn) {
+    // create array with root node
+    let children = [this.root];
+    // iterate through array, while arr has elements 
+    while (children.length !== 0) {
+      let node = children.shift();
+      // ...operator, push all elements in array, not entire array or use for loop and push
+      children.push(...node.children);
+      // call function on node
+      fn(node);
+    }
+  }
+
+  traverseDF (fn) {
+    let children = [this.root];
+
+    while (children.length !== 0) {
+      // remove first node
+      let node = children.shift();
+      // add children to start of array
+      children.unshift(...node.children)
+      // pass node into function
+      fn(node);
+    }
+  }
+}
+
+module.exports = { Tree, Node}
