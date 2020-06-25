b9a4eb31a5c7068fa3f61a301bcdc015a6d3f0d0
Noel Mendoza <nmendozavd@gmail.com>
2020-06-25 14:59:37 -0700

    completed BST validate exercise

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,27 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  // node.data should not be greater than max, node.data should not be less than min, if they are something is wrong
+  // left hand side, is node greater than max?
+  if (max !== null && node.data > max) {
+    return false
+  }
+  // right hand side, is node less than min?
+  if (min !== null && node.data < min) {
+    return false;
+  }
+  
+  // left side, recursion
+  if (node.left !== null && !validate(node.left, min, node.data)) {
+    return false;
+  }
+  // right side recursion 
+  if (node.right !== null && !validate(node.right, node.data, max)) {
+    return false;
+  }
+
+  return true;
+}
 
 module.exports = validate;
