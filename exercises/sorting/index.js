0d5be808d4c66dd1377ae11789004ae72b0b66db
Noel Mendoza <nmendozavd@gmail.com>
2020-06-26 03:01:06 -0700

    completed bubble sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,7 +2,19 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+  // find the largest record, and move it to right-hand side, and decrease the length by i
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j+1]) {
+        // swap values, great goes to the right side
+        // create temp to store element
+        let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+  return arr;
 }
 
 function selectionSort(arr) {
