3d7035a8283c8fd28864ed7cc64d8a8c94c0a5eb
Noel Mendoza <nmendozavd@gmail.com>
2020-06-26 03:26:17 -0700

    completed selection sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,32 +1,48 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
-function bubbleSort(arr) {
+function bubbleSort (arr) {
   // find the largest record, and move it to right-hand side, and decrease the length by i
+  // nested loops
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - i; j++) {
-      if (arr[j] > arr[j+1]) {
+    // this loop goes until the last element, and decreases
+    for (let j = 0; j < (arr.length - i - 1); j++) {
+      if (arr[j] > arr[j + 1]) {
         // swap values, great goes to the right side
         // create temp to store element
-        let temp = arr[j];
-        arr[j] = arr[j+1];
-        arr[j+1] = temp;
+        let temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
       }
     }
   }
-  return arr;
+  return arr
 }
 
-function selectionSort(arr) {
+function selectionSort (arr) {
+  // prove me wrong algo
 
+  for (let i = 0; i < arr.length; i++) {
+    let indexofMin = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexofMin]) {
+        indexofMin = j;
+      }
+    }
+    if (i !== indexofMin) {
+      let temp = arr[indexofMin];
+      arr[indexofMin] = arr[i];
+      arr[i] = temp;
+    }
+  }
+    return arr;
 }
 
-function mergeSort(arr) {
-
+function mergeSort (arr) {
 }
 
-function merge(left, right) {
-
+function merge (left, right) {
 }
 
-module.exports = { bubbleSort, selectionSort, mergeSort, merge };
+module.exports = { bubbleSort, selectionSort, mergeSort, merge}
